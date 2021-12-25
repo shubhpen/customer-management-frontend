ec094b552f80be11a37338fdb92cf7286be4c0a5
Shubham <shubham@example.com>
2021-12-26 00:19:39 +0530

    got data from Customer Rest API and shown on the UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ListCustomersComponent from './components/ListCustomerComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderComponent/>
+        <div className="container">
+          <ListCustomersComponent/>
+        </div>
+      <FooterComponent/>
     </div>
   );
 }
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className= "navbar navbar-expand-md navbar-dark bg-dark">
+                        <div>
+                            <a href="https://tekvision.in/" className="navbar-brand">Customer Management App</a>
+                        </div>
+                    </nav>
+                </header>
+                
+            </div>
+        );
+    }
+}
+
+export default HeaderComponent;
